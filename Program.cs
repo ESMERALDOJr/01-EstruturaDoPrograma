5b324d1a538338dca9938ae7e0d8ee1e1723d261
ESMERALDOJr <esmeraldo.log@hotmail.com>
2021-12-27 13:47:07 -0300

    CSharp

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EstruturaDoPrograma;
+
+namespace EstruturaDoPrograma
+{    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+
+            s.Empilha(0);
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            s.Empilha(1000);
+            s.Empilha(10000);
+            s.Empilha(100000);
+            s.Empilha(1000000);
+            
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine("Desempilhamento Realizado com Sucesso!!!");
+                
+        }
+    }
+}
